afed76fe3a9e77a108fe9d16dcac149cfc881eb5
cakyus <cakyus@gmail.com>
2013-07-24 03:59:26 +0700

    get storage id on object initiation

--- Storage.php
+++ Storage.php
@@ -6,15 +6,45 @@
 
 class Storage {
 	
+	// database handler
+	private $db;
+	
+	// storage id
+	private $storageId;
+	
 	public function __construct($name) {
-
+		
+		// check library dependency
+		if (class_exists('SQLite3') == false) {
+			throw new \Exception('SQLite3 extension is required');
+		}
+		
+		$this->db = new SQLite3(
+			 $_ENV['STORAGE_DATABASE']
+			,SQLITE3_OPEN_READWRITE
+			);
+		
+		$query = $this->db->querySingle(
+			 ' SELECT id FROM object_store'
+			.' WHERE name = "'.$this->db->escapeString($name).'"
+			');
+		
+		if (is_null($query)) {
+			$this->db->exec(
+				 ' INSERT INTO object_store (name)'
+				.' VALUES ("'.$this->db->escapeString($name).'")'
+			);
+			$this->storageId = $this->db->lastInsertRowID();
+		} else {
+			$this->storageId = $query;
+		}
 	}
 	
-	public function put($data, $key=null) {
+	public function put($object, $key=null) {
 		
 	}
 	
-	public function set($data, $key) {
+	public function set($object, $key) {
 		
 	}
 	
