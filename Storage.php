f155c567526a2a6fe3a44ada1ab1cc66a5467026
cakyus <cakyus@gmail.com>
2013-07-24 01:52:37 +0700

    initial commit

---
+++ Storage.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Simple data storage
+ **/
+
+class Storage {
+	
+	public function __construct($name) {
+
+	}
+	
+	public function put($data, $key=null) {
+		
+	}
+	
+	public function set($data, $key) {
+		
+	}
+	
+	public function get($key) {
+		
+	}
+	
+	public function del($key) {
+		
+	}
+}
